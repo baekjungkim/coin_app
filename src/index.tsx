c32b9f3a63880c47370bbfbbc51d8daf1f2086f0
Hongsinwon <ghdtlsdnjs12@naver.com>
2022-06-10 11:07:59 +0900

    io

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { RecoilRoot } from "recoil";
+import React from 'react';
+import * as ReactDOMClient from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
 
-import App from "./App";
+import App from './App';
 
 const queryClient = new QueryClient();
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOMClient.createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
     </RecoilRoot>
-  </React.StrictMode>,
-
-  document.getElementById("root")
+  </React.StrictMode>
 );
+
+//npm i react@next react-dom@next
